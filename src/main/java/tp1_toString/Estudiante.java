58b8b8eea634e367ebecdd0918f254c37ade1707
unknown <sebastiansaez44@gmail.com>
2025-09-06 13:54:38 -0300

    Primer commit tp1

---
+++ src/main/java/tp1_toString/Estudiante.java
@@ -0,0 +1,15 @@
+package tp1_toString;
+
+public class Estudiante extends Persona{
+    private String carrera;
+
+    public Estudiante(String nombre, int edad, String carrera){
+        super(nombre, edad);
+        this.carrera = carrera;
+    }
+
+    @Override
+    public String toString(){
+        return "Estudiante[ nombre: " + getNombre() + "', edad: " + getEdad() + ", carrera: ";
+    }
+}
---
+++ src/main/java/tp1_toString/Main.java
@@ -0,0 +1,17 @@
+package tp1_toString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Estudiante> lista = new ArrayList<>();
+
+        lista.add(new Estudiante("Juan", 20, "Ingenieria en Sistemas"));
+        lista.add(new Estudiante("Maria", 22, "Diseño Grafico"));
+        lista.add(new Estudiante("Pedro", 21, "Medicina"));
+
+        System.out.println(lista);
+    }
+}
