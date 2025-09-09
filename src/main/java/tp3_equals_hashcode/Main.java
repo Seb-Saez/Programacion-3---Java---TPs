2b9e5725833298d97c231f6d6fe891e3fcad8a4b
unknown <sebastiansaez44@gmail.com>
2025-09-08 23:47:30 -0300

    Tp4 con equals y contains

---
+++ src/main/java/tp3_equals_hashcode/Main.java
@@ -0,0 +1,34 @@
+package tp3_equals_hashcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Producto> productos = new ArrayList<>();
+
+        Producto p1 = new Producto("a1", "Arroz", 15);
+        Producto p2 = new Producto("a2", "Fideos", 20);
+        Producto p3 = new Producto("a1", "Arroz negro", 18);
+
+
+        agregarProducto(productos, p1);
+        agregarProducto(productos, p2);
+        agregarProducto(productos, p3);
+
+       // System.out.println(productos);
+
+        for (Producto p : productos){
+            System.out.println(p);
+        }
+
+        }
+        public static void agregarProducto(List<Producto> lista, Producto nuevoP){
+            if (!lista.contains(nuevoP)) {
+                lista.add(nuevoP);
+            } else{
+                System.out.println("El prducto no puede ser agregado porque esta duplicado " + nuevoP.getNombre());
+            }
+            }
+}
---
+++ src/main/java/tp3_equals_hashcode/Producto.java
@@ -0,0 +1,58 @@
+package tp3_equals_hashcode;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+@Getter
+public class Producto {
+    private String codigo;
+    private String nombre;
+    private int precio;
+
+    public Producto(String codigo, String nombre, int precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    // toString
+  /*  @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo=" + codigo +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }*/
+
+    @Override
+    public String toString(){
+        return "Producto: " +
+                "{ Codigo: " + codigo +
+                " Nombre: " + nombre +
+                " Precio: " + precio + " }";
+    }
+
+
+
+    // equals y hashcode
+
+    @Override
+    public boolean equals(Object o){
+        // si tienen misma dir de mem, entonces son iguales, si son de clases diff son distintos
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Producto product = (Producto) o;
+        return Objects.equals(codigo, product.codigo);
+
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hashCode(codigo);
+    }
+}
